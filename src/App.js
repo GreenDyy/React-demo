a9266ba1702f02753cca1b638fe16428cd96c77f
Huỳnh Khánh Duy <duyxanh2002@gmail.com>
2024-03-06 14:13:21 +0700

    FirtsCommit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Duy đẹp trai học React</p>
         <a
           className="App-link"
           href="https://reactjs.org"
